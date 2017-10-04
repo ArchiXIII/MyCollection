c99e4ae2b381d07f422e9814f3bf0de773cb4276
ArchiXIII <archi.xiii@mail.ru>
2017-10-04 21:27:48 +0400

    ...

--- src/main/java/ru/archi/Controller.java
+++ src/main/java/ru/archi/Controller.java
@@ -4,4 +4,16 @@ package main.java.ru.archi;
  * Created by Черный on 03.10.2017.
  */
 public class Controller {
+    public static void main(String[] args) {
+        MyList myList = new MyList();
+        myList.addPredicate(5, 9, 4);
+        myList.add(1);
+        myList.add(2);
+        myList.add(5);
+        myList.add(7);
+        myList.add(9);
+
+
+        System.out.println(myList.get(2) + "  " + myList.get(3));
+    }
 }
--- src/main/java/ru/archi/MyList.java
+++ src/main/java/ru/archi/MyList.java
@@ -1,7 +1,23 @@
 package main.java.ru.archi;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Created by Черный on 03.10.2017.
  */
-public class MyList {
+public class MyList extends ArrayList {
+    ArrayList<Integer> predicate = new ArrayList<>();
+
+    public void addPredicate(Integer...args) {
+        Collections.addAll(predicate, args);
+    }
+
+    public ArrayList<Integer> getPredicate(){
+        return predicate;
+    }
+
+    public void setPredicate(ArrayList predicate){
+        this.predicate = predicate;
+    }
 }
